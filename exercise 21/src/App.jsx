6c6c755686fa49d0ac683d031ccef8afd0684190
bahjaisack <bahjaisack@gmail.com>
2026-07-25 07:13:50 -0700

    adding exercise17,18,21

---
+++ exercise 21/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StudentDashboard from './StudentDashboard'
+
+const App = () => {
+  return (
+    <div>
+      <StudentDashboard/>
+    </div>
+  )
+}
+
+export default App
